a07715036714a9db89dfece59cd0b15aaac92caa
Bartosz Wos <bartosz.wos.cs@gmail.com>
2026-03-17 08:32:15 +0100

    feat: added blockchain class and implemented methods

---
+++ include/Blockchain.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "Block.hpp"
+#include <vector>
+#include <cstdint>
+
+class Blockchain{
+public:
+	std::vector<Block> chain;
+	uint32_t difficulty;
+
+public:
+	Blockchain(uint32_t difficulty);
+	Block getLastBlock() const;
+	void addBlock(Block new_block);
+	bool isValid() const;
+};
